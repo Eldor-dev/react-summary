d37f034553fbd08dd8a4a213bdfda32fbae54c64
Eldor Azimov <eldorazimov37@gmail.com>
2023-02-26 20:23:50 +0900

    Updating State Based on Previous State

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,30 +6,20 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 const PostsList = (props) => {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
   return (
     <>
       {props.isPosting && (
         <Modal onClose={props.onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={props.onStopPosting}
-          />
+          <NewPost onCancel={props.onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
